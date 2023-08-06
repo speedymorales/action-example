f3e5100015cbf0fc488530ff92ba0629d99c8df1
Jose Morales <speedymorales5@gmail.com>
2023-08-06 15:06:06 -0400

    docker deployment

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,8 @@ import * as WebSocket from 'ws';
 
 const app = express();
 
-app.get('/hello', (req, res) => {
-  res.send("Hello, World!");
+app.get('/api/hello', (req, res) => {
+  res.send('Hello, World!');
 });
 
 //initialize a simple http server
@@ -15,21 +15,21 @@ const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
 
 wss.on('connection', (ws: WebSocket) => {
+  //connection is up, let's add a simple simple event
+  ws.on('message', (message: string) => {
+    //log the received message and send it back to the client
+    console.log('received: %s', message);
+    ws.send(`Hello, you sent -> ${message}`);
+  });
 
-    //connection is up, let's add a simple simple event
-    ws.on('message', (message: string) => {
-
-        //log the received message and send it back to the client
-        console.log('received: %s', message);
-        ws.send(`Hello, you sent -> ${message}`);
-    });
-
-    //send immediatly a feedback to the incoming connection    
-    ws.send('Hi there, I am a WebSocket server. DB_HOST is: ' + process.env.DB_HOST);
+  //send immediatly a feedback to the incoming connection
+  ws.send(
+    'Hi there, I am a WebSocket server. DB_HOST is: ' + process.env.DB_HOST
+  );
 });
 
 //start our server
 const port = process.env.PORT || 3000;
 server.listen(port, () => {
-    console.log(`Server started on port ${port} :)`);
+  console.log(`Server started on port ${port} :)`);
 });
